6f4b0fa4142476073144ba3abae14e252131e286
Maxwell Taylor <S3931467@student.rmit.edu.au>
2026-04-16 15:14:49 +0100

    changelog: - set up structure

--- Backend/app.py
+++ Backend/app.py
@@ -1,3 +1,35 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import os, csv
 
 app = Flask(__name__)
+
+#Add Product Route
+app.route("/add_product", methods=["POST"])
+def add_product():
+    return
+
+#Read Products Route
+app.route("/read_products")
+def read_products():
+    return
+
+#Delete Product Route
+app.route("/delete_product", methods=["DELETE"])
+def delete_product():
+    return
+
+#Update Products Route
+app.route("/update_product", methods=["PUT"])
+def update_product():
+    return
+
+def read_file():
+    
+
+    infile = request.files['products.csv']
+    rtfile = os.fdopen(
+        infile.stream.fileno(), 'rt')
+    csvreader = csv.reader(rtfile)
+
+
+
